983eafe38f2dff2c960d3180b4575573db9dd3f3
saarif23 <arif23s.001@gmail.com>
2024-01-04 11:05:48 +0600

    update category sections

--- src/Components/HomeComponents/Categories.jsx
+++ src/Components/HomeComponents/Categories.jsx
@@ -21,17 +21,14 @@ const Categories = () => {
         <div className="">
             <div className="text-center  max-w-6xl mx-auto max-md:my-10 md:mt-10">
                 <h3
-                 data-aos="fade-down-right"
+                 data-aos="zoom-in"
                  data-aos-offset="200"
                  data-aos-delay="50"
                  data-aos-duration="1000"
-                className="text-4xl font-bold pb-3  ">Famous Book Categories</h3>
+                className="text-4xl font-bold pb-3 text-[#176B87]">Famous Book Categories</h3>
                 <p 
-                 data-aos="fade-down-left"
-                 data-aos-offset="200"
-                 data-aos-delay="50"
-                 data-aos-duration="1000"
-                className="max-md:text-sm">A library can be called a store–house of knowledge. In dictionaries the word “library” has been defined as “a building or room containing a collection of books”. A library plays a very important role in promoting the progress of knowledge. Many people love reading but not all can afford to buy books which are priced very high.</p>
+               
+                className="max-md:text-sm w-2/3 mx-auto">A library can be called a store–house of knowledge. In dictionaries the word “library” has been defined as “a building or room containing a collection of books”. A library plays a very important role in ...</p>
                 <div 
                  data-aos="fade-up"
                  data-aos-offset="200"
